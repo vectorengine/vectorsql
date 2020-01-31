caad983f3745eb5740c81e10d74f5ef24ace5028
BohuTANG <overred.shuttler@gmail.com>
2020-01-31 09:51:45 +0800

    datablocks: remove seqs if only one column to sort

--- src/datablocks/datablock_sort.go
+++ src/datablocks/datablock_sort.go
@@ -27,52 +27,20 @@ func NewSorter(col string, direction string) Sorter {
 }
 
 func (block *DataBlock) Sort(sorters ...Sorter) error {
-	zipFunc, err := functions.FunctionFactory("ZIP")
-	if err != nil {
-		return err
-	}
+	if block.NumColumns() == 1 {
+		cv := block.values[0]
+		matrix := cv.values
+		sorter := sorters[0]
 
-	// Seqs column.
-	max := block.NumRows()
-	seqs := make([]*datatypes.Value, max)
-	for i := 0; i < max; i++ {
-		seqs[i] = datatypes.ToValue(i)
-	}
-
-	// Sort columns.
-	var tuples []*datatypes.Value
-	for _, sorter := range sorters {
-		cv, ok := block.valuesmap[sorter.column]
-		if !ok {
+		if cv.column.Name != sorter.column {
 			return errors.Errorf("Can't find column:%v", sorter.column)
 		}
-		tuples = append(tuples, datatypes.MakeTuple(cv.values...))
-	}
-	tuples = append(tuples, datatypes.MakeTuple(seqs...))
-
-	// Zip.
-	if err := zipFunc.Validator.Validate(tuples...); err != nil {
-		return err
-	}
-	result, err := zipFunc.Logic(tuples...)
-	if err != nil {
-		return err
-	}
-
-	// Sort.
-	matrix := result.AsSlice()
-	sort.Slice(matrix[:], func(i, j int) bool {
-		irows := matrix[i].AsSlice()
-		jrows := matrix[j].AsSlice()
-		for x := 0; x < len(irows)-1; x++ {
-			cmp, err := datatypes.Compare(irows[x], jrows[x])
+		sort.Slice(matrix[:], func(i, j int) bool {
+			cmp, err := datatypes.Compare(matrix[i], matrix[j])
 			if err != nil {
 				return false
 			}
-			if cmp == datatypes.Equal {
-				continue
-			}
-			switch strings.ToUpper(sorters[x].direction) {
+			switch strings.ToUpper(sorter.direction) {
 			case "ASC":
 				return cmp == datatypes.LessThan
 			case "DESC":
@@ -80,15 +48,71 @@ func (block *DataBlock) Sort(sorters ...Sorter) error {
 			default:
 				return cmp == datatypes.LessThan
 			}
+		})
+	} else {
+		zipFunc, err := functions.FunctionFactory("ZIP")
+		if err != nil {
+			return err
 		}
-		return false
-	})
 
-	// Final.
-	finalSeqs := make([]*datatypes.Value, max)
-	for i, tuple := range matrix {
-		finalSeqs[i] = tuple.AsSlice()[len(sorters)]
+		// Seqs column.
+		max := block.NumRows()
+		seqs := make([]*datatypes.Value, max)
+		for i := 0; i < max; i++ {
+			seqs[i] = datatypes.ToValue(i)
+		}
+
+		// Sort columns.
+		var tuples []*datatypes.Value
+		for _, sorter := range sorters {
+			cv, ok := block.valuesmap[sorter.column]
+			if !ok {
+				return errors.Errorf("Can't find column:%v", sorter.column)
+			}
+			tuples = append(tuples, datatypes.MakeTuple(cv.values...))
+		}
+		tuples = append(tuples, datatypes.MakeTuple(seqs...))
+
+		// Zip.
+		if err := zipFunc.Validator.Validate(tuples...); err != nil {
+			return err
+		}
+		result, err := zipFunc.Logic(tuples...)
+		if err != nil {
+			return err
+		}
+
+		// Sort.
+		matrix := result.AsSlice()
+		sort.Slice(matrix[:], func(i, j int) bool {
+			irows := matrix[i].AsSlice()
+			jrows := matrix[j].AsSlice()
+			for x := 0; x < len(irows)-1; x++ {
+				cmp, err := datatypes.Compare(irows[x], jrows[x])
+				if err != nil {
+					return false
+				}
+				if cmp == datatypes.Equal {
+					continue
+				}
+				switch strings.ToUpper(sorters[x].direction) {
+				case "ASC":
+					return cmp == datatypes.LessThan
+				case "DESC":
+					return cmp == datatypes.GreaterThan
+				default:
+					return cmp == datatypes.LessThan
+				}
+			}
+			return false
+		})
+
+		// Final.
+		finalSeqs := make([]*datatypes.Value, max)
+		for i, tuple := range matrix {
+			finalSeqs[i] = tuple.AsSlice()[len(sorters)]
+		}
+		block.setSeqs(finalSeqs)
 	}
-	block.setSeqs(finalSeqs)
 	return nil
 }
