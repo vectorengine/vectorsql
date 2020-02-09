b19346867e60af0cb31f36250b3a8c72bffb4e07
BohuTANG <overred.shuttler@gmail.com>
2020-02-09 10:47:22 +0800

    expressions: add condition expressions #10

---
+++ src/expressions/expression_conditions.go
@@ -0,0 +1,137 @@
+// Copyright 2020 The VectorSQL Authors.
+//
+// Code is licensed under Apache License, Version 2.0.
+
+package expressions
+
+import (
+	"datavalues"
+)
+
+func LT(left interface{}, right interface{}) IExpression {
+	exprs := expressionsFor(left, right)
+	return &BinaryExpression{
+		name:     "<",
+		left:     exprs[0],
+		right:    exprs[1],
+		validate: All(),
+		eval: func(left *datavalues.Value, right *datavalues.Value) (*datavalues.Value, error) {
+			cmp, err := datavalues.Compare(left, right)
+			if err != nil {
+				return nil, err
+			}
+			return datavalues.MakeBool(cmp == datavalues.LessThan), nil
+		},
+	}
+}
+
+func LTE(left interface{}, right interface{}) IExpression {
+	exprs := expressionsFor(left, right)
+	return &BinaryExpression{
+		name:     "<=",
+		left:     exprs[0],
+		right:    exprs[1],
+		validate: All(),
+		eval: func(left *datavalues.Value, right *datavalues.Value) (*datavalues.Value, error) {
+			cmp, err := datavalues.Compare(left, right)
+			if err != nil {
+				return nil, err
+			}
+			return datavalues.MakeBool(cmp < datavalues.GreaterThan), nil
+		},
+	}
+}
+
+func EQ(left interface{}, right interface{}) IExpression {
+	exprs := expressionsFor(left, right)
+	return &BinaryExpression{
+		name:     "=",
+		left:     exprs[0],
+		right:    exprs[1],
+		validate: All(),
+		eval: func(left *datavalues.Value, right *datavalues.Value) (*datavalues.Value, error) {
+			cmp, err := datavalues.Compare(left, right)
+			if err != nil {
+				return nil, err
+			}
+			return datavalues.MakeBool(cmp == datavalues.Equal), nil
+		},
+	}
+}
+
+func NEQ(left interface{}, right interface{}) IExpression {
+	exprs := expressionsFor(left, right)
+	return &BinaryExpression{
+		name:     "<>",
+		left:     exprs[0],
+		right:    exprs[1],
+		validate: All(),
+		eval: func(left *datavalues.Value, right *datavalues.Value) (*datavalues.Value, error) {
+			cmp, err := datavalues.Compare(left, right)
+			if err != nil {
+				return nil, err
+			}
+			return datavalues.MakeBool(cmp != datavalues.Equal), nil
+		},
+	}
+}
+
+func GT(left interface{}, right interface{}) IExpression {
+	exprs := expressionsFor(left, right)
+	return &BinaryExpression{
+		name:     ">",
+		left:     exprs[0],
+		right:    exprs[1],
+		validate: All(),
+		eval: func(left *datavalues.Value, right *datavalues.Value) (*datavalues.Value, error) {
+			cmp, err := datavalues.Compare(left, right)
+			if err != nil {
+				return nil, err
+			}
+			return datavalues.MakeBool(cmp == datavalues.GreaterThan), nil
+		},
+	}
+}
+
+func GTE(left interface{}, right interface{}) IExpression {
+	exprs := expressionsFor(left, right)
+	return &BinaryExpression{
+		name:     ">=",
+		left:     exprs[0],
+		right:    exprs[1],
+		validate: All(),
+		eval: func(left *datavalues.Value, right *datavalues.Value) (*datavalues.Value, error) {
+			cmp, err := datavalues.Compare(left, right)
+			if err != nil {
+				return nil, err
+			}
+			return datavalues.MakeBool(cmp > datavalues.LessThan), nil
+		},
+	}
+}
+
+func AND(left interface{}, right interface{}) IExpression {
+	exprs := expressionsFor(left, right)
+	return &BinaryExpression{
+		name:     "AND",
+		left:     exprs[0],
+		right:    exprs[1],
+		validate: All(),
+		eval: func(left *datavalues.Value, right *datavalues.Value) (*datavalues.Value, error) {
+			return datavalues.ToValue(left.AsBool() && right.AsBool()), nil
+		},
+	}
+}
+
+func OR(left interface{}, right interface{}) IExpression {
+	exprs := expressionsFor(left, right)
+	return &BinaryExpression{
+		name:     "OR",
+		left:     exprs[0],
+		right:    exprs[1],
+		validate: All(),
+		eval: func(left *datavalues.Value, right *datavalues.Value) (*datavalues.Value, error) {
+			return datavalues.ToValue(left.AsBool() || right.AsBool()), nil
+		},
+	}
+}
---
+++ src/expressions/expression_conditions_test.go
@@ -0,0 +1,120 @@
+// Copyright 2020 The VectorSQL Authors.
+//
+// Code is licensed under Apache License, Version 2.0.
+
+package expressions
+
+import (
+	"testing"
+
+	"datavalues"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConditionsExpression(t *testing.T) {
+	tests := []struct {
+		name      string
+		expr      IExpression
+		expect    *datavalues.Value
+		errstring string
+	}{
+		{
+			name:   "a<b",
+			expr:   LT("a", "b"),
+			expect: datavalues.ToValue(true),
+		},
+		{
+			name:   "b<a",
+			expr:   LT("b", "a"),
+			expect: datavalues.ToValue(false),
+		},
+		{
+			name:   "a<(b+1)",
+			expr:   LT("a", ADD("b", 1)),
+			expect: datavalues.ToValue(true),
+		},
+		{
+			name:   "b<=b",
+			expr:   LTE("b", "b"),
+			expect: datavalues.ToValue(true),
+		},
+		{
+			name:   "a=a",
+			expr:   EQ("a", "a"),
+			expect: datavalues.ToValue(true),
+		},
+		{
+			name:   "a=b",
+			expr:   EQ("a", "b"),
+			expect: datavalues.ToValue(false),
+		},
+		{
+			name:   "a<>a",
+			expr:   NEQ("a", "a"),
+			expect: datavalues.ToValue(false),
+		},
+		{
+			name:   "a<>b",
+			expr:   NEQ("a", "b"),
+			expect: datavalues.ToValue(true),
+		},
+		{
+			name:   "b>a",
+			expr:   GT("b", "a"),
+			expect: datavalues.ToValue(true),
+		},
+		{
+			name:   "a>b",
+			expr:   GT("a", "b"),
+			expect: datavalues.ToValue(false),
+		},
+		{
+			name:   "a>=a",
+			expr:   GTE("a", "a"),
+			expect: datavalues.ToValue(true),
+		},
+		{
+			name:   "(a<b) AND (c<d)",
+			expr:   AND(LT("c", "d"), LT("c", "d")),
+			expect: datavalues.ToValue(true),
+		},
+		{
+			name:   "(a<b) AND (c>d)",
+			expr:   AND(LT("c", "d"), GT("c", "d")),
+			expect: datavalues.ToValue(false),
+		},
+		{
+			name:   "(a<b) OR (c<d)",
+			expr:   OR(LT("c", "d"), LT("c", "d")),
+			expect: datavalues.ToValue(true),
+		},
+		{
+			name:   "(a>b) OR (c>d)",
+			expr:   OR(GT("c", "d"), GT("c", "d")),
+			expect: datavalues.ToValue(false),
+		},
+	}
+
+	for _, test := range tests {
+		params := Map{
+			"a": datavalues.ToValue(1),
+			"b": datavalues.ToValue(2),
+			"c": datavalues.ToValue("c"),
+			"d": datavalues.ToValue("d"),
+		}
+		actual, err := test.expr.Update(params)
+		if test.errstring != "" {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+			assert.Equal(t, test.expect, actual)
+
+			_, _ = test.expr.Get()
+			err = test.expr.Walk(func(e IExpression) (bool, error) {
+				return true, nil
+			})
+			assert.Nil(t, err)
+		}
+	}
+}
